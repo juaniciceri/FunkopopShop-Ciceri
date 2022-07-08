fe4fabb773e85b78b99b7a30500a960a61448721
carlos ciceri <juanncice@gmail.com>
2022-07-08 17:49:05 -0300

    Carrito de compras realizado

---
+++ src/components/Data.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import img1 from '../img/img1.png';
+import img2 from '../img/img2.jpg';
+import img3 from '../img/img3.jpg';
+
+const Data = {
+    productData:[
+        {
+            id:1,
+            img:img1,
+            title: 'Doctor Octopus',
+            desc:'',
+            price: 8.500,
+        },
+        {
+            id:2,
+            img:img2,
+            title: 'Avenngers Hulk',
+            desc:'',
+            price: 7.780,
+        },
+        {
+            id:3,
+            img:img3,
+            title: 'IronMan',
+            desc:'',
+            price: 7.300,
+        }
+    ],
+};
+
+export default Data;
---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ItemCard from "./ItemCard";
+import Data from "./Data";
+const Home = () => {
+    return (
+        <>
+           <h1 className="text-center mt-3">Productos</h1>
+              <section className="py-4 container">
+                    <div className="row justify-content-center">
+                        {Data.productData.map((item, index)=>{
+                            return(
+                                <ItemCard 
+                                  img={item.img}
+                                  title={item.title}
+                                  desc={item.desc} 
+                                  price={item.price} 
+                                  item={item} 
+                                  key={index} 
+                                />
+                            )
+                        })}
+
+
+
+                    </div>
+              </section>
+        </>
+    );
+};
+
+export default Home;
